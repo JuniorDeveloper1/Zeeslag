504e4f5b33a86e31e947015433430d33f6c40cbe
JuniorDeveloper <nickje.debut@gmail.com>
2023-12-12 19:39:09 +0100

    Added GameManager

--- src/main/java/Zeeslag/GameManager.java
+++ src/main/java/Zeeslag/GameManager.java
@@ -1,2 +1,77 @@
-package Zeeslag;public class GameManager {
+package Zeeslag;
+
+import com.almasb.fxgl.entity.GameWorld;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class GameManager {
+    private GameManager(){}
+
+    private Game game;
+
+    private Player player1;
+
+    private Player player2;
+
+    /**
+     * We kunnen hier Highscoore, Aantal clicks, ... adden
+     */
+
+    private static GameManager gameManager;
+
+    public static GameManager getInstance(){
+        if(GameManager.gameManager == null){
+            GameManager.gameManager = new GameManager();
+            gameManager.setGame(new Game());
+        }
+
+        return GameManager.gameManager;
+    }
+
+    public Player getPlayer1() {
+        return player1;
+    }
+
+    public void setPlayer1(Player player1) {
+        this.player1 = player1;
+    }
+
+    public Player getPlayer2() {
+        return player2;
+    }
+
+    public void setPlayer2(Player player2) {
+        this.player2 = player2;
+    }
+
+    public Game getGame() {
+        return game;
+    }
+
+    private void setGame(Game game) {
+        this.game = game;
+    }
+
+    /**
+     * Further game logic
+     */
+
+    public void openScene(String fxmlFile, String title) throws IOException {
+        Parent root = FXMLLoader.load(GameManager.class.getClassLoader().getResource(fxmlFile));
+        Stage stage = new Stage();
+        if(title == null) {title = "Default";}
+        stage.setTitle(title);
+        stage.setScene(new Scene(root));
+        stage.show();
+    }
+
+    public void closeScene(Button button){
+        Stage currentStage = (Stage) button.getScene().getWindow();
+        currentStage.close();
+    }
 }
--- src/main/java/Zeeslag/Main.java
+++ src/main/java/Zeeslag/Main.java
@@ -7,27 +7,9 @@ import javafx.stage.Stage;
 
 
 public class Main extends Application {
-
-    public static void main(String[] args){
-
-        Game game = new Game();
-        game.loadDefault();
-
-
-        Player player1 = new Player("Nick", game.getBoard1());
-
-        Player player2 = new Player("Noah", game.getBoard1());
-
-
-        player1.attack(player2, 5 , 4);
-
-
-        //THIS IS WRONG you player2 cannot attack player2
-    }
-
     @Override
     public void start(Stage stage) throws Exception {
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("main_menu.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Zeeslag");
         stage.setScene(scene);
