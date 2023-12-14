07c9a69c7635eb0b2433f17f56b3d2068a0da11b
JuniorDeveloper <nickje.debut@gmail.com>
2023-12-14 15:06:44 +0100

    JavaFX

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module Zeeslag {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires com.almasb.fxgl.all;
+
+    opens Zeeslag to javafx.fxml;
+    opens Zeeslag.Controllers to javafx.fxml;
+    exports Zeeslag;
+}
